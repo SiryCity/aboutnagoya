6a1fd9078624f9228a66c514d1d95641ce65f980
rm <sirycity2018@gmail.com>
2019-05-28 12:30:02 +0900

    blog部分終了

---
+++ components/PrevNextLink.vue
@@ -0,0 +1,73 @@
+<template lang="pug">
+  nuxt-link.prev-next-link__wrapper(:to='{name:"posts-date", params:{date}}')
+    div.prev-next-link__caption {{type === 'prev' ? '前の記事' : '次の記事'}}
+    div.prev-next-link
+      img.prev-next-link__img(:src='img')
+      div.prev-next-link__meta {{date}} {{district}}
+      h3.prev-next-link__title {{title}}
+      h4.prev-next-link__sub-title {{subTitle}}
+</template>
+
+<script>
+export default {
+  props: ['type', 'date', 'title', 'subTitle', 'district','img']
+}
+</script>
+
+
+<style lang="stylus" scoped>
+
+.prev-next-link__wrapper
+  width 100%
+  height 130px
+  color var(--c)
+  text-decoration none
+  border-radius 3px
+  margin-bottom var(--margin)
+  display flex
+  flex-wrap wrap
+  box-shadow 0 3px 6px #bbb
+
+  .prev-next-link__caption
+    width 100%
+    height 30px
+    line-height 30px
+    color var(--bgc)
+    background var(--c)
+    text-align center
+    box-sizing border-box
+    border-bottom 4px solid var(--gold)
+  .prev-next-link
+    width 80%
+    height 100px
+    display flex
+    flex-direction column
+    flex-wrap wrap
+    justify-content center
+    align-content flex-start
+
+    &__img
+      width 90px
+      height 90px
+      margin 5px 3px
+      
+    &__meta
+      height 15px
+      font-size 14px
+    &__title
+      height 40px
+      line-height 50px
+      font-size 20px
+      margin 0
+      padding 0
+      font-weight normal
+    &__sub-title
+      height 25px
+      line-height 30px
+      font-size 14px
+      margin 0
+      padding 0
+      font-weight normal
+
+</style>
+
--- pages/posts/_date.vue
+++ pages/posts/_date.vue
@@ -1,12 +1,45 @@
 <template lang="pug">
   page-wrapper
+
     section-wrapper(pos='left')
       title-box(:title='nearbyPosts.curr.title')
+
+      hr
+      
       p {{nearbyPosts.curr.subTitle}}
       p {{nearbyPosts.curr.date}}
       p {{nearbyPosts.curr.district}}
       div(v-html='$md.render(nearbyPosts.curr.body)')
+
+      hr
+
+      prev-next-link(
+        v-if='nearbyPosts.prev'
+        type='prev'
+
+        :date='nearbyPosts.prev.date'
+        :subTitle='nearbyPosts.prev.subTitle'
+        :title='nearbyPosts.prev.title'
+        :district='nearbyPosts.prev.district'
+
+        :img='kari'
+      )
+
+      prev-next-link(
+        v-if='nearbyPosts.next'
+        type='next'
+
+        :date='nearbyPosts.next.date'
+        :subTitle='nearbyPosts.next.subTitle'
+        :title='nearbyPosts.next.title'
+        :district='nearbyPosts.next.district'
+
+        :img='kari'
+      )
+      
+
     section-wrapper(pos='right')
+
       title-box(:title='`${nearbyPosts.curr.district}の投稿`')
       article-link(
         v-for='(post, i) in nearbyPosts.closer'
@@ -19,6 +52,7 @@
 
         :img='kari'
       )
+
       title-box(title='最新の投稿')
       article-link(
         v-for='(post, i) in nearbyPosts.latest'
@@ -41,6 +75,8 @@ import PageWrapper from '~/components/PageWrapper'
 import SectionWrapper from '~/components/SectionWrapper'
 import TitleBox from '~/components/TitleBox'
 import ArticleLink from '~/components/ArticleLink'
+import PrevNextLink from '~/components/PrevNextLink'
+
 import kari from '~/assets/nagoya.svg'
 export default {
   components:{
@@ -48,6 +84,7 @@ export default {
     SectionWrapper,
     TitleBox,
     ArticleLink,
+    PrevNextLink,
   },
 
 
